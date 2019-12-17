4357a7ebc50f3232f72342e9d8c89518d7bfc0e3
Bryce <bryce.senninger@gmail.com>
2019-12-16 18:32:18 -0700

    updates to bio page

--- client/src/components/bio/index.jsx
+++ client/src/components/bio/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Selfie from "../../assets/selfie.png";
 
 function Bio() {
   return (
@@ -13,55 +14,51 @@ function Bio() {
           <Link to="/">Go Back</Link>
         </div>
       </div>
-      <div class="">
-        <div class="jumbotron bioPageJumbo">
-          <p class="headP">
-            I am a full-stack developer specialized in HTML, CSS, JavaScript,
-            React, Node.js, Express, MySQL, and MongoDB.
+      <div className="carousel-content2">
+        <div className="jumbotron bioJumbo pastJumbo">
+          <h1 className="display-4">Past</h1>
+          <hr className="my-4" />
+          <p>
+            I began my post-secondary education at the University of Utah
+            studying Economics and Statistical Analysis. Eventually I came to
+            the conclusion that Economics might not be the best way to explore
+            my passion for problem solving and creative reasoning.
           </p>
           <p>
-            Previous to coding, I was a student at the University of Utah. I
-            entered the University of Utah with 60 credit hours applied to my
-            degree from completion of the International Baccalaureate program
-            and AP testing in high school, which unfortunately meant I didn't
-            have much time to explore my interests in college. Rather quickly I
-            found myself diving into upper level classes for my declared major,
-            Economics with an Emphasis on Statistical Analysis.
+            I started coding and knew right away I had found a passion, and I
+            haven't looked back since.
           </p>
-          <img
-            class="selfie"
-            src={require("../../assets/selfie.png")}
-            alt="Bryce"
-          />
+        </div>
+      </div>
+      <div className="carousel-content3">
+        <div className="jumbotron projectJumbo presentJumbo">
+          <h1 className="display-4">Present</h1>
+          <hr className="my-4" />
           <p>
-            As I began to worry this field might not be my calling, I also found
-            myself increasingly intrigued by the projects my friends in the CS
-            department were working on.
+            One thing I find so exciting about coding is that the applications
+            are limitless, and the learning never stops.
           </p>
           <p>
-            I decided to take a step away from the path in front of me and
-            explore this curiousity in coding, and began tinkering around with
-            HTML, CSS, and JavaScript in the fall of 2018. Immediately I fell in
-            love with the aspects of problem solving and implemenation of logic.
-            Knowing that this was the path that I wanted to take, I decided to
-            enroll in the Full-Stack Development Professional Education Camp
-            offered through the University of Utah.
+            These days I enjoy working on projects that allow me to combine my
+            interest in coding with other passions. Whether it be outdoor
+            recreation, literature, film, cooking, or something else entirely- I
+            love the process of creating applications that make it easier for
+            people to track, share, and connect.
           </p>
+          <img class="selfie" src={Selfie} alt="Bryce" />
+        </div>
+      </div>
+      <div className="carousel-content4">
+        <div className="jumbotron contactJumbo futureJumbo">
+          <h1 className="display-4">Future</h1>
+          <hr className="my-4" />
           <p>
-            The experience from the bootcamp was incredible, very rapidly
-            exposing me to many different technologies and giving me the tools
-            to take my code to the next level. Running through full front-end
-            design, back-end database creation, and the necessary logic
-            intersecting the two at a break-neck pace was a thrilling
-            experience- and one that taught me quickly how important it is in
-            the field of coding to be able to learn new concepts and adapt
-            rapidly.
-          </p>
-          <p>
-            I pride myself on my problem solving and learning capabilities, and
-            having had them thorougly put to the test and discovering a passion
-            in the process has been life-changing for me. I can't wait to see
-            what the future holds for me in this exciting field.
+            My goal is to continue learning and exploring my passion for coding
+            through a continuous commitment to developing my skills as a
+            programmer and expanding my area of knowledge within the field. I am
+            seeking new opportunites that allow me to utilize my skills in
+            problem solving and creative reasoning while also challenging me to
+            grow.
           </p>
         </div>
       </div>
--- client/src/components/contact/index.jsx
+++ client/src/components/contact/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LinkedIn from "../../assets/linkedin-sign.png";
+import GitHub from "../../assets/github-logo(1).png";
 
 function Contact() {
   return (
@@ -9,58 +11,36 @@ function Contact() {
           <h1 class="display-4">Bryce Senninger</h1>
           <p class="lead">Full-Stack Developer</p>
           <hr class="my-4" />
-          <h2>Contact Information</h2>
+          <h2>Contact Info</h2>
           <Link to="/">Go Back</Link>
         </div>
       </div>
-      <div class="">
-        <div class="jumbotron bioPageJumbo">
-          <p class="headP">
-            I am a full-stack web developer specialized in HTML, CSS,
-            Javascript, React, Node.js, Express, MySQL, and MongoDB.
-          </p>
-          <p>
-            Previous to coding, I was a student at the University of Utah. I
-            entered the University of Utah with 60 credit hours applied to my
-            degree from completion of the International Baccalaureate program
-            and AP testing in high school, which unfortunately meant I didn't
-            have much time to explore my interests in college. Rather quickly I
-            found myself diving into upper level classes for my declared major,
-            Economics with an Emphasis on Statistical Analysis.
-            <img
-              class="selfie"
-              src="../assets/156906134636719205ds.jpg"
-              alt="Bryce"
-            />
-            As I began to worry this field might not be my calling, I also found
-            myself increasingly intrigued by the projects my friends in the CS
-            department were working on.
-          </p>
-          <p>
-            I decided to take a step away from the path in front of me and
-            explore this curiousity in coding, and began tinkering around with
-            HTML and CSS in the fall of 2018. Immediately I fell in love with
-            the aspects of problem solving and implemenation of logic. Knowing
-            that this was the path that I wanted to take, I decided to enroll in
-            the Full-Stack Web Development Bootcamp offered through the
-            University of Utah.
-          </p>
-          <p>
-            The experience from the bootcamp was incredible, very rapidly
-            exposing me to many different technologies and giving me the tools
-            to take my code to the next level. Running through full front-end
-            design, back-end database creation, and the necessary logic
-            intersecting the two at a break-neck pace was a thrilling
-            experience- and one that taught me quickly how important it is in
-            the field of coding to be able to learn new concepts and adapt
-            rapidly.
-          </p>
-          <p>
-            I pride myself on my problem solving and learning capabilities, and
-            having had them thorougly put to the test and discovering a passion
-            in the process has been life-changing for me. I can't wait to see
-            what the future holds for me in this exciting field.
-          </p>
+      <div className="carousel-content2">
+        <div className="jumbotron bioJumbo pastJumbo">
+          <img class="logo" src={LinkedIn} alt="Bryce" />
+        </div>
+      </div>
+      <div className="carousel-content3">
+        <div className="jumbotron projectJumbo presentJumbo">
+          {/* <h1 className="display-4">Present</h1>
+          <hr className="my-4" /> */}
+          <form method="POST">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" />
+
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" />
+
+            <label htmlFor="message">Message</label>
+            <textarea name="message" rows="3"></textarea>
+
+            <input type="submit" />
+          </form>
+        </div>
+      </div>
+      <div className="carousel-content4">
+        <div className="jumbotron contactJumbo futureJumbo">
+          <img class="logo" src={GitHub} alt="Bryce" />
         </div>
       </div>
     </>
